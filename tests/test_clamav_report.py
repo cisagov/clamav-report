6019d465f5eb504cd51b9cb73d234f495e549691
Felddy <markf@geekpad.com>
2019-09-09 15:30:59 -0400

    Update tests.

--- tests/test_clamav_report.py
+++ tests/test_clamav_report.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env pytest -vs
-"""Tests for example."""
+"""Tests for clamav_report."""
 
 import logging
 import os
@@ -8,15 +8,7 @@ from unittest.mock import patch
 
 import pytest
 
-import example
-
-div_params = [
-    (1, 1, 1),
-    (2, 2, 1),
-    (0, 1, 0),
-    (8, 2, 4),
-    pytest.param(0, 0, 0, marks=pytest.mark.xfail(raises=ZeroDivisionError)),
-]
+from clamav_report import clamav_report
 
 log_levels = (
     "debug",
@@ -29,14 +21,14 @@ log_levels = (
 
 # define sources of version strings
 TRAVIS_TAG = os.getenv("TRAVIS_TAG")
-PROJECT_VERSION = example.__version__
+PROJECT_VERSION = clamav_report.__version__
 
 
 def test_stdout_version(capsys):
     """Verify that version string sent to stdout agrees with the module version."""
     with pytest.raises(SystemExit):
         with patch.object(sys, "argv", ["bogus", "--version"]):
-            example.example.main()
+            clamav_report.main()
     captured = capsys.readouterr()
     assert (
         captured.out == f"{PROJECT_VERSION}\n"
@@ -56,40 +48,15 @@ def test_release_version():
 @pytest.mark.parametrize("level", log_levels)
 def test_log_levels(level):
     """Validate commandline log-level arguments."""
-    with patch.object(sys, "argv", ["bogus", f"--log-level={level}"]):
+    with patch.object(
+        sys, "argv", ["bogus", f"--log-level={level}", "tests/inventory.txt", "out.csv"]
+    ):
         with patch.object(logging.root, "handlers", []):
             assert (
                 logging.root.hasHandlers() is False
             ), "root logger should not have handlers yet"
-            return_code = example.example.main()
+            return_code = clamav_report.main()
             assert (
                 logging.root.hasHandlers() is True
             ), "root logger should now have a handler"
             assert return_code == 0, "main() should return success (0)"
-
-
-@pytest.mark.parametrize("dividend, divisor, quotient", div_params)
-def test_division(dividend, divisor, quotient):
-    """Verify division results."""
-    result = example.example_div(dividend, divisor)
-    assert result == quotient, "result should equal quotient"
-
-
-@pytest.mark.slow
-def test_slow_division():
-    """Example of using a custom marker.
-
-    This test will only be run if --runslow is passed to pytest.
-    Look in conftest.py to see how this is implemented.
-    """
-    import time
-
-    result = example.example_div(256, 16)
-    time.sleep(4)
-    assert result == 16, "result should equal be 16"
-
-
-def test_zero_division():
-    """Verify that division by zero throws the correct exception."""
-    with pytest.raises(ZeroDivisionError):
-        example.example_div(1, 0)
