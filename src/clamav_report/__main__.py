c4699417da57930e11ef06442acaf7249fb7aab6
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2026-01-26 20:47:23 -0500

    Modify whitespace
    This gets rid of some errors from our black pre-commit hook.
    
    Also let isort do its thing.

--- src/clamav_report/__main__.py
+++ src/clamav_report/__main__.py
@@ -1,10 +1,12 @@
 """Code to run if this package is used as a Python module."""
 
+# Standard Python Libraries
 import logging
+
 from .clamav_report import main
 
 try:
-	main()
+    main()
 finally:
     # Stop logging and clean up
-	logging.shutdown()
+    logging.shutdown()
